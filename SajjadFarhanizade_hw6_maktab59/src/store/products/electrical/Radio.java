61328551fad1e93d0c91c968fc691c213ffb5e5e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-10 22:40:22 +0430

    Getter & Setter added to Radio.java int antennaRange, SignalModulationMode signal initialized in Radio.java

--- SajjadFarhanizade_hw6_maktab59/src/store/products/electrical/Radio.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/electrical/Radio.java
@@ -1,4 +1,29 @@
 package store.products.electrical;
 
-public class Radio {
+public class Radio extends ElectricalAppliance {
+    int antennaRange;
+    SignalModulationMode signal;
+
+    public Radio(int id, String name, int price, Voltage voltage, PowerSource powerSource,
+                 int powerConsumption, int antennaRange, SignalModulationMode signal) {
+        super(id, name, price, voltage, powerSource, powerConsumption);
+        this.antennaRange = antennaRange;
+        this.signal = signal;
+    }
+
+    public int getAntennaRange() {
+        return antennaRange;
+    }
+
+    public void setAntennaRange(int antennaRange) {
+        this.antennaRange = antennaRange;
+    }
+
+    public SignalModulationMode getSignal() {
+        return signal;
+    }
+
+    public void setSignal(SignalModulationMode signal) {
+        this.signal = signal;
+    }
 }
