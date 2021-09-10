76429b060b2b8f9fd051cd7da07c50444caa40cb
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-10 22:14:50 +0430

    Voltage voltage, PowerSource powerSource, int powerConsumption initialized in ElectricalAppliance.java. The Constructor updated for initializing the new variables.

--- SajjadFarhanizade_hw6_maktab59/src/store/products/electrical/ElectricalAppliance.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/electrical/ElectricalAppliance.java
@@ -1,4 +1,16 @@
 package store.products.electrical;
 
-public class ElectricalAppliance {
+import store.products.Product;
+
+public class ElectricalAppliance extends Product {
+    private Voltage voltage;
+    private PowerSource powerSource;
+    private int powerConsumption;
+
+    public ElectricalAppliance(int id, String name, int price, Voltage voltage, PowerSource powerSource, int powerConsumption) {
+        super(id, name, price);
+        this.voltage = voltage;
+        this.powerSource = powerSource;
+        this.powerConsumption = powerConsumption;
+    }
 }
