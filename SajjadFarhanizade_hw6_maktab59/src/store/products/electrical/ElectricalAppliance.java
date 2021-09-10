cb93864240650b5b5cad625fc18ad437b1c7616e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-10 22:16:16 +0430

    Setter & Getter added to ElectricalAppliance.java

--- SajjadFarhanizade_hw6_maktab59/src/store/products/electrical/ElectricalAppliance.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/electrical/ElectricalAppliance.java
@@ -13,4 +13,28 @@ public class ElectricalAppliance extends Product {
         this.powerSource = powerSource;
         this.powerConsumption = powerConsumption;
     }
+
+    public Voltage getVoltage() {
+        return voltage;
+    }
+
+    public void setVoltage(Voltage voltage) {
+        this.voltage = voltage;
+    }
+
+    public PowerSource getPowerSource() {
+        return powerSource;
+    }
+
+    public void setPowerSource(PowerSource powerSource) {
+        this.powerSource = powerSource;
+    }
+
+    public int getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public void setPowerConsumption(int powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
 }
