fe5943d062aac0f94031074c0bd22f18718bf267
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-10 22:51:22 +0430

    Constructor added to Television.java Getter & Setter added to Television.java Tv_type tvType, int sizeInInch, int[] resolution initialized in Television.java

--- SajjadFarhanizade_hw6_maktab59/src/store/products/electrical/Television.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/electrical/Television.java
@@ -1,4 +1,39 @@
 package store.products.electrical;
 
-public class Television {
+public class Television extends ElectricalAppliance{
+    TV_Type tvType;
+    int sizeInInch;
+    int[] resolution = new int[2];
+
+    public Television(int id, String name, int price, Voltage voltage, PowerSource powerSource,
+                      int powerConsumption, TV_Type tvType, int sizeInInch, int[] resolution) {
+        super(id, name, price, voltage, powerSource, powerConsumption);
+        this.tvType = tvType;
+        this.sizeInInch = sizeInInch;
+        this.resolution = resolution;
+    }
+
+    public TV_Type getTvType() {
+        return tvType;
+    }
+
+    public void setTvType(TV_Type tvType) {
+        this.tvType = tvType;
+    }
+
+    public int getSizeInInch() {
+        return sizeInInch;
+    }
+
+    public void setSizeInInch(int sizeInInch) {
+        this.sizeInInch = sizeInInch;
+    }
+
+    public int[] getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(int[] resolution) {
+        this.resolution = resolution;
+    }
 }
