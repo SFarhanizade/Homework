bbae101c9b37a96516196b33ac02bf6eb02f9b45
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-11 17:58:38 +0430

    First commit

--- SajjadFarhanizade_hw6_maktab59/src/store/Customer.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/Customer.java
@@ -1,4 +1,77 @@
 package store;
 
 public class Customer {
+    private String userName;
+    private String passWord;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+private Address address;
+
+    public Customer(String userName, String passWord, String firstName, String lastName, String phone, String email, Address address) {
+        this.userName = userName;
+        this.passWord = passWord;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
