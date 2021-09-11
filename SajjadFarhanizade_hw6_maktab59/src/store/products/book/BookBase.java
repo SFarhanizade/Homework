4a78d137389b9f7b6c97c879b364e55f65526571
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-11 13:09:14 +0430

    Constructor, Getter & Setter added to BookBase.java

--- SajjadFarhanizade_hw6_maktab59/src/store/products/book/BookBase.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/book/BookBase.java
@@ -1,4 +1,40 @@
 package store.products.book;
 
-public class BookBase {
+import store.products.Product;
+
+public class BookBase extends Product {
+    private Subject subject;
+    private int pageNumber;
+    private String publisher;
+
+    public BookBase(int id, String name, int price, Subject subject, int pageNumber, String publisher) {
+        super(id, name, price);
+        this.subject = subject;
+        this.pageNumber = pageNumber;
+        this.publisher = publisher;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 }
