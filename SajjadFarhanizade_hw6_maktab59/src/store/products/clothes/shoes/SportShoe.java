60e3c3215a0aaf740a6340e03b6086af9d1b4515
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-11 12:52:04 +0430

    Constructor added to SportShoe.java

--- SajjadFarhanizade_hw6_maktab59/src/store/products/clothes/shoes/SportShoe.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/clothes/shoes/SportShoe.java
@@ -1,4 +1,11 @@
 package store.products.clothes.shoes;
 
-public class SportShoe {
+import attribute.Color;
+import attribute.Gender;
+import attribute.Material;
+
+public class SportShoe extends Shoe{
+    public SportShoe(int id, String name, int price, int size, Gender gender, Color color, Material material) {
+        super(id, name, price, size, gender, color, material);
+    }
 }
