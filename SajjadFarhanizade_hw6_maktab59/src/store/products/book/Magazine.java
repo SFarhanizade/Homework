57745839797b9e6f8ff648213bd0abd94eff6337
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-11 13:09:47 +0430

    Constructor, Getter & Setter added to Magazine.java

--- SajjadFarhanizade_hw6_maktab59/src/store/products/book/Magazine.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/book/Magazine.java
@@ -1,4 +1,17 @@
 package store.products.book;
 
-public class Magazine {
+public class Magazine extends BookBase {
+    private int number;
+
+    public Magazine(int id, String name, int price, Subject subject, int pageNumber, String publisher) {
+        super(id, name, price, subject, pageNumber, publisher);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
