9463be87b316aeab421a3cdfe69054c61a475b93
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-11 12:45:38 +0430

    Constructor, Getter & Setter added to Shoe.java

--- SajjadFarhanizade_hw6_maktab59/src/store/products/clothes/shoes/Shoe.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/clothes/shoes/Shoe.java
@@ -1,4 +1,51 @@
 package store.products.clothes.shoes;
 
-public class Shoe {
+import store.products.Product;
+import attribute.*;
+
+public class Shoe extends Product {
+        int size;
+        Gender gender;
+        Color color;
+        Material material;
+
+    public Shoe(int id, String name, int price, int size, Gender gender, Color color, Material material) {
+        super(id, name, price);
+        this.size = size;
+        this.gender = gender;
+        this.color = color;
+        this.material = material;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
 }
