6811bfaf5bd18c0b1c50f4ffcc9e33214f0465f3
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-11 12:50:58 +0430

    Constructor added to FormalShoe.java

--- SajjadFarhanizade_hw6_maktab59/src/store/products/clothes/shoes/FormalShoe.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/products/clothes/shoes/FormalShoe.java
@@ -1,4 +1,11 @@
 package store.products.clothes.shoes;
 
-public class FormalShoe {
+import attribute.Color;
+import attribute.Gender;
+import attribute.Material;
+
+public class FormalShoe extends Shoe{
+    public FormalShoe(int id, String name, int price, int size, Gender gender, Color color, Material material) {
+        super(id, name, price, size, gender, color, material);
+    }
 }
