66375e4ada34b784c46c352433eaf2dcbb68ff0a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-13 15:16:21 +0430

    login and logout methods added

--- SajjadFarhanizade_hw6_maktab59/src/store/Login.java
+++ SajjadFarhanizade_hw6_maktab59/src/store/Login.java
@@ -4,11 +4,12 @@ public class Login {
     private Customer signedInCustomer;
     private String userName;
     private String passWord;
+    private boolean isLoggedIn = false;
 
     public Login(Customer[] customers, String userName, String passWord) {
         boolean isLoggedIn = false;
         for (int i = 0; i < customers.length; i++) {
-            if(customers[i].getUserName()== userName && customers[i].getPassWord()==passWord){
+            if(customers[i].getUserName().equals(userName) && customers[i].getPassWord().equals(passWord)){
                 signedInCustomer = customers[i];
                 isLoggedIn = true;
                 break;
@@ -20,6 +21,32 @@ public class Login {
         }
         this.userName = userName;
         this.passWord = passWord;
+    }
+
+    public Login(Customer[] customers, Object[] user) {
+        for (int i = 0; i < customers.length; i++) {
+            if(customers[i].getUserName().equals(user[0].toString()) && customers[i].getPassWord().equals(user[1].toString())){
+                signedInCustomer = customers[i];
+                isLoggedIn = true;
+                break;
+            }
+        }
+        if(!isLoggedIn){
+            System.out.println("Wrong UserName or Password!");
+            return;
+        }
+        this.userName = user[0].toString();
+        this.passWord = user[1].toString();
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void logOut() {
+         signedInCustomer = null;
+         passWord = null;
+         userName = null;
     }
 
     public Customer getSignedInCustomer(){
