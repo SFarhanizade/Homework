eed465be3f3e6c180c9532fb09a3a901c79a11a3
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-09-24 22:57:44 +0330

    Initial Commit

---
+++ SajjadFarhanizade_hw8_maktab59/Turn/src/DemoTurners2.java
@@ -0,0 +1,9 @@
+public class DemoTurners2 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Year year = new Year();
+
+        car.turn();
+        year.turn();
+    }
+}
---
+++ SajjadFarhanizade_hw8_maktab59/Turn/src/Year.java
@@ -0,0 +1,6 @@
+public class Year implements Turner{
+    @Override
+    public void turn() {
+        System.out.println("New Year!");
+    }
+}
