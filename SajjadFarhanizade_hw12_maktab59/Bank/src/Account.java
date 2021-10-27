9adf1efcbb2f2ed12d9f79886dcf7d1e94262a9c
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-10-27 20:37:37 +0330

    ATM_Transactions added.

--- SajjadFarhanizade_hw12_maktab59/Bank/src/Account.java
+++ SajjadFarhanizade_hw12_maktab59/Bank/src/Account.java
@@ -1,6 +1,7 @@
 public class Account {
     public int number;
     public int balance;
+    public ATM_Transaction[] atm_transactions;
     public void deposit(){}
     public void withdraw(){}
     void createTransaction(){}
