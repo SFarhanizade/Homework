2222cd2c5c53bbcef6a240fbb8dfdcd09a995cfc
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-10-27 20:35:55 +0330

    Attributes and behavior added.

--- SajjadFarhanizade_hw12_maktab59/Bank/src/ATM_Transaction.java
+++ SajjadFarhanizade_hw12_maktab59/Bank/src/ATM_Transaction.java
@@ -1,5 +1,10 @@
 import java.util.Date;
 
 public class ATM_Transaction {
-
+    public int transactionID;
+    public Date date;
+    public String type;
+    public int amount;
+    public int postBalance;
+    public void modifies(){}
 }
