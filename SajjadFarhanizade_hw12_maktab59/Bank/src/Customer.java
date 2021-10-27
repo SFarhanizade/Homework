922e39e9acd8d0b9772d248365fe9c65fc75c975
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-10-27 20:32:42 +0330

    Attributes and behavior added.

--- SajjadFarhanizade_hw12_maktab59/Bank/src/Customer.java
+++ SajjadFarhanizade_hw12_maktab59/Bank/src/Customer.java
@@ -1,2 +1,11 @@
+import java.util.Date;
+
 public class Customer {
+    public String name;
+    public String address;
+    public Date dob;
+    public String cardNumber;
+    public String pin;
+    public void verifyPassword(){}
+    public Account[] accounts;
 }
