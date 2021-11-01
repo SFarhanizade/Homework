c0033864bf58b4c540944e2441b93dded0c02414
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-01 11:49:49 +0330

    All crud methods completed

--- SajjadFarhanizade_hw13_maktab59/JdbcExample_2/src/main/java/ir/maktab/jdbc/service/AbstractCrudService.java
+++ SajjadFarhanizade_hw13_maktab59/JdbcExample_2/src/main/java/ir/maktab/jdbc/service/AbstractCrudService.java
@@ -3,6 +3,8 @@ package ir.maktab.jdbc.service;
 import ir.maktab.jdbc.dao.core.BaseDao;
 import ir.maktab.jdbc.entity.base.BaseEntity;
 
+import java.util.List;
+
 public class AbstractCrudService<T extends BaseEntity<ID>, ID extends Number> {
     private BaseDao<T, ID> baseDao;
 
@@ -12,6 +14,18 @@ public class AbstractCrudService<T extends BaseEntity<ID>, ID extends Number> {
         } else {
             baseDao.update(entity.getId(), entity);
         }
+    }
+
+    public T getById(ID id){
+        return baseDao.loadById(id);
+    }
+
+    public List<T> getAll(){
+        return baseDao.loadAll();
+    }
+
+    public void delete(T entity){
+        baseDao.delete(entity.getId());
     }
 
     public void setBaseDao(BaseDao<T, ID> baseDao) {
--- SajjadFarhanizade_hw13_maktab59/JdbcExample_2/src/main/java/ir/maktab/jdbc/service/StudentService.java
+++ SajjadFarhanizade_hw13_maktab59/JdbcExample_2/src/main/java/ir/maktab/jdbc/service/StudentService.java
@@ -5,6 +5,8 @@ import ir.maktab.jdbc.dao.StudentDao;
 import ir.maktab.jdbc.dao.core.BaseDao;
 import ir.maktab.jdbc.entity.Student;
 
+import java.util.List;
+
 public class StudentService extends AbstractCrudService<Student, Integer> {
     public StudentService() {
         setBaseDao(new StudentDao());
@@ -13,5 +15,21 @@ public class StudentService extends AbstractCrudService<Student, Integer> {
     public StudentDao getBaseDao() {
         return (StudentDao) super.getBaseDao();
     }
+    public void add(Student student){
+        saveOrUpdate(student);
+    }
+
+    public Student getById(Integer id){
+        return super.getById(id);
+    }
+
+    public List<Student> getAll(){
+        return super.getAll();
+    }
+
+    public void delete(Student student){
+        super.delete(student);
+    }
+
 
 }
