21689057c60e0ae508593071b1cd7ed4cd0c52c5
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-01 11:57:01 +0330

    All crud methods completed

--- SajjadFarhanizade_hw13_maktab59/JdbcExample_2/src/main/java/ir/maktab/jdbc/service/CourseService.java
+++ SajjadFarhanizade_hw13_maktab59/JdbcExample_2/src/main/java/ir/maktab/jdbc/service/CourseService.java
@@ -1,4 +1,36 @@
 package ir.maktab.jdbc.service;
 
-public class CourseService {
+import ir.maktab.jdbc.dao.CourseDao;
+import ir.maktab.jdbc.entity.Course;
+
+import java.util.List;
+
+public class CourseService extends AbstractCrudService<Course,Integer> {
+    public CourseService() {
+        super.setBaseDao(new CourseDao());
+    }
+
+    @Override
+    public void saveOrUpdate(Course entity) {
+        super.saveOrUpdate(entity);
+    }
+
+    @Override
+    public Course getById(Integer integer) {
+        return super.getById(integer);
+    }
+
+    @Override
+    public List<Course> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public void delete(Course entity) {
+        super.delete(entity);
+    }
+
+    public CourseDao getBaseDao() {
+        return (CourseDao) super.getBaseDao();
+    }
 }
