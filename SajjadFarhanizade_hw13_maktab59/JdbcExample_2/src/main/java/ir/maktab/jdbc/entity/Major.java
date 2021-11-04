2267124bc09513ebef645b884e6fe482ecda3a9f
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-04 11:34:57 +0330

    builder field changed. toString method added.

--- SajjadFarhanizade_hw13_maktab59/JdbcExample_2/src/main/java/ir/maktab/jdbc/entity/Major.java
+++ SajjadFarhanizade_hw13_maktab59/JdbcExample_2/src/main/java/ir/maktab/jdbc/entity/Major.java
@@ -30,31 +30,39 @@ public class Major implements BaseEntity<Integer> {
 
     @Override
     public void setId(Integer integer) {
-   this.id = id;
+        this.id = id;
     }
 
     @Override
     public Integer getId() {
         return id;
     }
-    public static MajorBuilder builder(){
+
+    public static MajorBuilder builder() {
         return new MajorBuilder();
     }
-    public static class MajorBuilder{
+
+    public static class MajorBuilder {
         private Integer id;
         private String name;
-        public MajorBuilder id(int id){
+
+        public MajorBuilder id(int id) {
             this.id = id;
             return this;
         }
 
-        public MajorBuilder name(String name){
-            this.name= name;
+        public MajorBuilder name(String name) {
+            this.name = name;
             return this;
         }
 
-        public Major build(){
+        public Major build() {
             return new Major(id, name);
         }
+    }
+
+    @Override
+    public String toString() {
+        return (name == null)? "":name;
     }
 }
