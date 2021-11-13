f4f888f18fde47b6ee1daf0f518432e171f94573
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-13 15:16:30 +0330

    Algorithm implemented

--- SajjadFarhanizade_hw14_maktab59/StreamSort/src/Main.java
+++ SajjadFarhanizade_hw14_maktab59/StreamSort/src/Main.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>(Arrays.asList("Amir","Hatef","Mehran",
+                "Mojtaba","Mohammad","Ali","Davood","Reza","Mohsen"));
+        Map<Integer, List<String>> groupedWords =words.stream()
+                .collect(Collectors.groupingBy(s -> s.length(),Collectors.mapping(s ->s,Collectors.toList())));
+        System.out.println(groupedWords);
+    }
 }
