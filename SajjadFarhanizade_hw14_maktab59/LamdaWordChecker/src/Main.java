6cfad58777e8075f6a1062da613d179e682e3f0e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-13 14:52:53 +0330

    First Commit

---
+++ SajjadFarhanizade_hw14_maktab59/LamdaWordChecker/src/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+	public static void main(String[] args){
+		FunctionalInterface func = (s) -> s.startsWith(Character.toString(s.charAt(s.length()-1)));
+		System.out.println(func.checkIfLastFirstSame("SOS"));
+	}
+}
