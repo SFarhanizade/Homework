407876c6edd4b39c2b3d51663b4e87977bb00cfe
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-20 10:36:10 +0330

    checker of empty input added

--- SajjadFarhanizade_hw15_maktab59/StringToIntConvert/src/main/java/StringToIntConverter.java
+++ SajjadFarhanizade_hw15_maktab59/StringToIntConvert/src/main/java/StringToIntConverter.java
@@ -1,5 +1,7 @@
 public class StringToIntConverter {
     public static int convert(String s) {
+        if(s.equals(""))
+            throw new IllegalArgumentException("The input shouldn't be empty");
         return Integer.parseInt(s);
     }
 }
