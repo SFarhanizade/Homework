796e5d43cbcb90e658e64f73be2e9d6c6ed072e1
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-20 10:28:36 +0330

    Simple String to int convert test

--- SajjadFarhanizade_hw15_maktab59/StringToIntConvert/src/test/java/StringToIntConverterTest.java
+++ SajjadFarhanizade_hw15_maktab59/StringToIntConvert/src/test/java/StringToIntConverterTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringToIntConverterTest {
 
+    @Test
+    void testStringToInt(){
+        assertEquals(123,StringToIntConverter.convert("123"));
+    }
 }
