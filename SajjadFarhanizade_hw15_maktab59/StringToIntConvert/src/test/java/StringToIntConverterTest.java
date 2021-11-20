4c8330583731b2ffc1b5cb8afa395226d734edb2
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-20 10:29:54 +0330

    test method name changed

--- SajjadFarhanizade_hw15_maktab59/StringToIntConvert/src/test/java/StringToIntConverterTest.java
+++ SajjadFarhanizade_hw15_maktab59/StringToIntConvert/src/test/java/StringToIntConverterTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringToIntConverterTest {
 
     @Test
-    void testStringToInt(){
+    void test_String_To_Int(){
         assertEquals(123,StringToIntConverter.convert("123"));
     }
+
+    @Test
+    void
 }
