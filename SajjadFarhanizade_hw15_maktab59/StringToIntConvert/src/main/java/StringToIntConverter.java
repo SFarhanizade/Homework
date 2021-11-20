ff07f3d2081dd2c517fcd4c6a4c02fd3b014cb6b
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-20 10:40:12 +0330

    checker of containing space added.

--- SajjadFarhanizade_hw15_maktab59/StringToIntConvert/src/main/java/StringToIntConverter.java
+++ SajjadFarhanizade_hw15_maktab59/StringToIntConvert/src/main/java/StringToIntConverter.java
@@ -2,6 +2,8 @@ public class StringToIntConverter {
     public static int convert(String s) {
         if(s.equals(""))
             throw new IllegalArgumentException("The input shouldn't be empty");
+        if(s.contains(" "))
+            throw new IllegalArgumentException("The input shouldn't have any space");
         return Integer.parseInt(s);
     }
 }
