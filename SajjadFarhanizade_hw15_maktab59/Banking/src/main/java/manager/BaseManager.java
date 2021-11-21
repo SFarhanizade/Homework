c78927bb0e907da8870cdb063df341689305422d
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-21 15:16:32 +0330

    all methods and attributes added.

--- SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/manager/BaseManager.java
+++ SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/manager/BaseManager.java
@@ -1,13 +1,42 @@
 package manager;
 
+import dao.BaseDao;
 import entity.BaseEntity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.Set;
 
-public class BaseManager <T extends BaseEntity<ID>, ID extends String>{
+public class BaseManager<T extends BaseEntity<ID>, ID extends String> {
     EntityManagerFactory entityManagerFactory =
             Persistence.createEntityManagerFactory("jpa-bank");
     EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    protected BaseDao baseDao;
+
+    public BaseManager(BaseDao baseDao) {
+        this.baseDao = baseDao;
+    }
+
+    public void save(T entity) {
+        baseDao.save(entity);
+    }
+
+    public void update(T entity) {
+        baseDao.update(entity);
+    }
+
+    public void delete(T entity) {
+        baseDao.delete(entity);
+    }
+
+    public T loadById(ID id){
+        return (T) baseDao.loadById(id);
+    }
+
+    public Set<T> loadAll(){
+        return baseDao.loadAll();
+    }
+
 }
