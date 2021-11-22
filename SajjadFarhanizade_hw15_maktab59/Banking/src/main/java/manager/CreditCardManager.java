16ccc77f3ef9dae32e7ce1ab9b4ca76c4ae6ce29
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-22 18:03:16 +0330

    wrong pin input checker added.

--- SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/manager/CreditCardManager.java
+++ SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/manager/CreditCardManager.java
@@ -4,17 +4,13 @@ import dao.CreditCardDao;
 import entity.Account;
 import entity.CreditCard;
 import entity.Transaction;
-import exception.CardNotExistsException;
-import exception.NotEnoughBalanceException;
-import exception.WrongExpDateException;
-import exception.WrongPasswordException;
+import exception.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.sql.Timestamp;
 
-public class CreditCardManager extends BaseManager<CreditCard,Integer>{
-
+public class CreditCardManager extends BaseManager<CreditCard, Integer> {
 
 
     public CreditCardManager(EntityManager entityManager) {
@@ -22,7 +18,7 @@ public class CreditCardManager extends BaseManager<CreditCard,Integer>{
         setBaseDao(new CreditCardDao(entityManager));
     }
 
-    public CreditCard loadByNumber(String number){
+    public CreditCard loadByNumber(String number) {
         TypedQuery<CreditCard> query =
                 entityManager.createQuery("From CreditCard c where c.number = :number", CreditCard.class);
         query.setParameter("number", number);
@@ -31,23 +27,23 @@ public class CreditCardManager extends BaseManager<CreditCard,Integer>{
 
     public boolean transferIsValid(String origin, String destination, Integer amount) throws CardNotExistsException, NotEnoughBalanceException {
         CreditCard originCreditCard = loadByNumber(origin);
-        if(originCreditCard==null)
+        if (originCreditCard == null)
             throw new CardNotExistsException("Origin card number doesn't exist!");
         CreditCard destCreditCard = loadByNumber(destination);
-        if(destCreditCard==null)
+        if (destCreditCard == null)
             throw new CardNotExistsException("Destination card number doesn't exist!");
-        boolean hasMoney = originCreditCard.getAccount().getBalance()>=amount;
-        if(originCreditCard.getAccount().getBalance()<amount)
+        boolean hasMoney = originCreditCard.getAccount().getBalance() >= amount;
+        if (originCreditCard.getAccount().getBalance() < amount)
             throw new NotEnoughBalanceException("Not enough balance!");
         return true;
 
     }
 
-    public void transfer(CreditCard origin, String destination, Integer amount){
+    public void transfer(CreditCard origin, String destination, Integer amount) {
         Account originAccount = origin.getAccount();
-        originAccount.setBalance(originAccount.getBalance()-(amount+600));
+        originAccount.setBalance(originAccount.getBalance() - (amount + 600));
         Account destAccount = loadByNumber(destination).getAccount();
-        destAccount.setBalance(destAccount.getBalance()+amount);
+        destAccount.setBalance(destAccount.getBalance() + amount);
         Transaction transaction = Transaction.builder()
                 .origin(originAccount)
                 .destination(destAccount)
@@ -59,24 +55,36 @@ public class CreditCardManager extends BaseManager<CreditCard,Integer>{
         new AccountManager(entityManager).save(destAccount);
     }
 
-    public void changePassword(CreditCard card, String newPassword) throws WrongExpDateException, WrongPasswordException, CardNotExistsException {
-            if(cardIsValid(card)) {
-                if(newPassword.length()!=4)
-                    throw new WrongPasswordException("Password length should be 4!");
-                card.setPin(newPassword);
-                update(card);
-            }
+    public void changePassword(CreditCard card, String newPassword) throws WrongExpDateException,
+            WrongPasswordException, CardNotExistsException, AccountIsLockedException {
+        if (cardIsValid(card)) {
+            if (newPassword.length() != 4)
+                throw new WrongPasswordException("Password length should be 4!");
+            card.setPin(newPassword);
+            update(card);
+        }
     }
 
-    public boolean cardIsValid(CreditCard creditCard) throws CardNotExistsException, WrongPasswordException, WrongExpDateException {
+    public boolean cardIsValid(CreditCard creditCard) throws CardNotExistsException, WrongPasswordException,
+            WrongExpDateException, AccountIsLockedException {
         CreditCard card = loadByNumber(creditCard.getNumber());
-        if(card==null)
+        if (card == null)
             throw new CardNotExistsException("Card doesn't exist!");
-        if(creditCard.getPin()!=card.getPin())
+        if(card.getAccount().isLocked())
+            throw new AccountIsLockedException("Account is locked!");
+        if (creditCard.getPin() != card.getPin()) {
+            if (card.getCountWrongPin() < 2) {
+                card.setCountWrongPin(card.getCountWrongPin() + 1);
+            } else {
+                card.setCountWrongPin(3);
+                card.getAccount().setLocked(true);
+                save(card);
+            }
             throw new WrongPasswordException("Wrong password!");
-        if (creditCard.getCvv()!=card.getCvv())
+        }
+        if (creditCard.getCvv() != card.getCvv())
             throw new WrongPasswordException("Wrong cvv!");
-        if(creditCard.getExpDate()!=card.getExpDate())
+        if (creditCard.getExpDate() != card.getExpDate())
             throw new WrongExpDateException("Wrong Expiry date!");
         return true;
     }
