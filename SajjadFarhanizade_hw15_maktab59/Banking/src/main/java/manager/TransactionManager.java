29d9c4baf9024b9b08e56395f317d86fcd0a5fcc
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-22 18:04:01 +0330

    getTransactions and getTransactionsAfter methods added.

--- SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/manager/TransactionManager.java
+++ SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/manager/TransactionManager.java
@@ -1,14 +1,38 @@
 package manager;
 
 import dao.TransactionDao;
+import entity.Account;
 import entity.Transaction;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class TransactionManager extends BaseManager<Transaction,Long>{
+public class TransactionManager extends BaseManager<Transaction, Long> {
 
     public TransactionManager(EntityManager entityManager) {
         super(entityManager);
         setBaseDao(new TransactionDao(entityManager));
     }
+
+    public List<Transaction> getTransactions(Account account) {
+        TypedQuery<Transaction> query = entityManager.createQuery("From Transaction t where t.origin=:account OR t.destination=:account",
+                Transaction.class);
+        query.setParameter("account", account);
+        return query.getResultList();
+    }
+    public List<Transaction> getTransactionsAfter(Account account, Timestamp timestamp) {
+        TypedQuery<Transaction> query = entityManager.createQuery(
+                "From Transaction t where (t.origin=:account OR t.destination=:account)" +
+                        "And t.timestamp >= :timestamp",
+                Transaction.class);
+        query.setParameter("account", account);
+        query.setParameter("timestamp", timestamp);
+        return query.getResultList();
+    }
+
+
 }
