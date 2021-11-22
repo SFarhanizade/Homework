73bcb5e00a8c7e53f4db0170633839f5a60c291c
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-22 17:23:07 +0330

    card_number auto-generation by card_id added.

--- SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/dao/CreditCardDao.java
+++ SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/dao/CreditCardDao.java
@@ -4,9 +4,18 @@ import entity.CreditCard;
 
 import javax.persistence.EntityManager;
 
-public class CreditCardDao extends BaseDao<CreditCard,Long>{
+public class CreditCardDao extends BaseDao<CreditCard,Integer>{
     public CreditCardDao(EntityManager entityManager) {
         super(entityManager);
+    }
+
+    @Override
+    public void save(CreditCard entity) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(entity);
+        entityManager.getTransaction().commit();
+        entity.setNumber(entity.getId()+6037_0000_0000_0000L+"");
+        update(entity);
     }
 
     @Override
