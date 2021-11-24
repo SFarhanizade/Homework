f282a37d729c6de84096b122d3c57ea59cd9a3ef
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-24 13:27:35 +0330

    exists and loadByNationalId methods added.

--- SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/dao/CustomerDao.java
+++ SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/dao/CustomerDao.java
@@ -3,11 +3,37 @@ package dao;
 import entity.Customer;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
-public class CustomerDao extends BaseDao<Customer,Long> {
+public class CustomerDao extends BaseDao<Customer, Long> {
 
     public CustomerDao(EntityManager entityManager) {
         super(entityManager);
+    }
+
+    public boolean exists(String nationalId) {
+        entityManager.getTransaction().begin();
+        TypedQuery<Customer> query =
+                entityManager.createQuery("From Customer c where c.nationalId=:nationalId",
+                        Customer.class);
+        query.setParameter("nationalId", nationalId);
+        List<Customer> result = query.getResultList();
+        entityManager.getTransaction().commit();
+        return result.size() > 0;
+    }
+
+    public Customer loadByNationalId(String nationalId) {
+        entityManager.getTransaction().begin();
+        TypedQuery<Customer> query =
+                entityManager.createQuery("From Customer c where c.nationalId=:nationalId",
+                        getEntityClass());
+        query.setParameter("nationalId", nationalId);
+        List<Customer> result = query.getResultList();
+        entityManager.getTransaction().commit();
+        if (result.size() == 1)
+            return result.get(0);
+        return null;
     }
 
     @Override
