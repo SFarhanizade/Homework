d48cdb1b15ee518827c4946d48f6a9c95c78fbde
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-24 13:23:52 +0330

    loadByName and exists methods added

--- SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/manager/BranchManager.java
+++ SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/manager/BranchManager.java
@@ -1,14 +1,27 @@
 package manager;
 
 import dao.BranchDao;
+import entity.Account;
 import entity.Branch;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class BranchManager extends BaseManager<Branch,Long>{
 
     public BranchManager(EntityManager entityManager) {
         super(entityManager);
         setBaseDao(new BranchDao(entityManager));
+    }
+
+    public Branch loadByName(String name){
+        TypedQuery<Branch> query = entityManager.createQuery("From Branch where name=:name", Branch.class);
+        query.setParameter("name",name);
+        return query.getSingleResult();
+    }
+
+    public boolean exists(String name){
+        return new BranchDao(entityManager).exists(name);
     }
 }
