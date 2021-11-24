fc5a6920e15e0a4729df908004802cf697aa5d65
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-24 13:23:28 +0330

    exists method added

--- SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/dao/BranchDao.java
+++ SajjadFarhanizade_hw15_maktab59/Banking/src/main/java/dao/BranchDao.java
@@ -3,10 +3,22 @@ package dao;
 import entity.Branch;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class BranchDao extends BaseDao<Branch,Long>{
     public BranchDao(EntityManager entityManager) {
         super(entityManager);
+    }
+
+    public boolean exists(String name){
+        entityManager.getTransaction().begin();
+        TypedQuery<Branch> query =
+                entityManager.createQuery("From Branch b where b.name=:name", getEntityClass());
+        query.setParameter("name", name);
+        List<Branch> result = query.getResultList();
+        entityManager.getTransaction().commit();
+        return result.size()>0;
     }
 
     @Override
