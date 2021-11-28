cb2075d947b1522b2c3c953a4c872c2482324744
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-28 16:37:31 +0330

    Initial Commit

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/BaseManager.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/BaseManager.java
@@ -1,4 +1,28 @@
 package manager;
 
-public class BaseManager {
+import dao.BaseDao;
+import entity.BaseEntity;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class BaseManager<T extends BaseEntity>{
+    private EntityManager entityManager;
+    BaseDao<T> baseDao;
+    public void save(T entity){
+        baseDao.save(entity);
+    }
+    public void update(T entity){
+        baseDao.update(entity);
+    }
+    public void delete(T entity){
+        baseDao.delete(entity);
+    }
+    public T loadById(Long id){
+        return baseDao.loadById(id);
+    }
+    public List<T> loadAll() {
+        return baseDao.loadAll();
+    }
+
 }
