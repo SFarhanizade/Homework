77b585fae50282b1ae9ffa49edeb8a62ee9a65f3
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-28 14:32:06 +0330

    team added.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/Coach.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/Coach.java
@@ -1,10 +1,27 @@
 package entity;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Coach extends BaseThing{
+
+    @OneToOne
+    private Team team;
+
     public Coach() {
     }
 
-    public Coach(Long id, String name) {
+    public Coach(Long id, String name, Team team) {
         super(id, name);
+        this.team = team;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/Player.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/Player.java
@@ -1,4 +1,27 @@
 package entity;
 
-public class Player {
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Player extends BaseThing{
+
+@ManyToOne
+private Team team;
+
+    public Player() {
+    }
+
+    public Player(Long id, String name, Team team) {
+        super(id, name);
+        this.team = team;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
