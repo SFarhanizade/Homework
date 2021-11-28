79334c28c96bba5438f4ee2828305e3453bdbc64
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-28 12:11:07 +0330

    name property with setter, getter and constructor added.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/BasePerson.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/BasePerson.java
@@ -1,4 +1,24 @@
 package entity;
 
-public class BasePerson {
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BasePerson extends BaseEntity{
+
+    @Column
+    protected String name;
+
+    public BasePerson(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
