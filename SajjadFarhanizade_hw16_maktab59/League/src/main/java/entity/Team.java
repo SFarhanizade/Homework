f54cbf06b1b2368161990e262829aaddfd725eb2
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-28 14:30:58 +0330

    coach, capitan and list of players added.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/Team.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/Team.java
@@ -1,4 +1,44 @@
 package entity;
 
-public class Team {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.List;
+
+@Entity
+public class Team extends BaseThing{
+
+    @OneToOne
+    private Coach coach;
+
+    @OneToMany
+    private List<Player> players;
+
+    @OneToOne
+    private Player capitan;
+
+    public Team() {
+    }
+
+    public Team(Long id, String name, Coach coach, List<Player> players) {
+        super(id, name);
+        this.coach = coach;
+        this.players = players;
+    }
+
+    public Coach getCoach() {
+        return coach;
+    }
+
+    public void setCoach(Coach coach) {
+        this.coach = coach;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
 }
