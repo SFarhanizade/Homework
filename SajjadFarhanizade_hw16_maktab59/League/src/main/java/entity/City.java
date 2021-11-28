10c21eec79554f16486019298ca0cede0c2eabcd
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-28 12:16:46 +0330

    extends BaseThing

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/City.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/City.java
@@ -1,4 +1,11 @@
 package entity;
 
-public class City {
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class City extends BaseThing{
+
+    public City(Long id, String name) {
+        super(id, name);
+    }
 }
