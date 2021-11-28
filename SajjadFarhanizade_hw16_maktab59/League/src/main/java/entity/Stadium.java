51f1f7dfef0b82fc7e470ac6648f23fcce2e86d5
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-28 12:25:46 +0330

    city property with setter, getter and constructor added.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/Stadium.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/Stadium.java
@@ -1,4 +1,32 @@
 package entity;
 
-public class Stadium {
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Stadium extends BaseThing{
+
+    @ManyToOne
+    private City city;
+
+    public Stadium(City city) {
+        this.city = city;
+    }
+
+    public Stadium(Long id, String name, City city) {
+        super(id, name);
+        this.city = city;
+    }
+
+    public Stadium() {
+
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
 }
