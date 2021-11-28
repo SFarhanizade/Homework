d65b913f709fe488d5bc2b89ebe4db41c14bff09
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-28 12:14:15 +0330

    name changed to BaseThing

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/BaseThing.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/BaseThing.java
@@ -1,4 +1,24 @@
 package entity;
 
-public class BaseThing {
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseThing extends BaseEntity{
+
+    @Column
+    protected String name;
+
+    public BaseThing(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
