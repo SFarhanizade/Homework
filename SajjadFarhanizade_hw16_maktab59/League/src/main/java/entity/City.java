b2b61478abe708ae00cf39636917a68d9b12d4ea
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-28 12:25:21 +0330

    list of stadium added

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/City.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/City.java
@@ -1,11 +1,29 @@
 package entity;
 
+import javax.persistence.Entity;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
+import java.util.List;
 
-@MappedSuperclass
+@Entity
 public class City extends BaseThing{
+
+    @OneToMany(mappedBy = "city")
+    private List<Stadium> stadiums;
 
     public City(Long id, String name) {
         super(id, name);
+    }
+
+    public City() {
+        super();
+    }
+
+    public List<Stadium> getStadiums() {
+        return stadiums;
+    }
+
+    public void setStadiums(List<Stadium> stadiums) {
+        this.stadiums = stadiums;
     }
 }
