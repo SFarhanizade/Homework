23a9eb3728d77fa4e194bcf2b3a257d85fc95d4e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-29 17:47:16 +0330

    static final entityManager added. transaction begin and commit added to methods.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/BaseManager.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/BaseManager.java
@@ -4,23 +4,38 @@ import dao.BaseDao;
 import entity.BaseEntity;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
-public class BaseManager<T extends BaseEntity>{
-    private EntityManager entityManager;
+public class BaseManager<T extends BaseEntity> {
+    static final EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("jpa-league");
+    public static final EntityManager entityManager = entityManagerFactory.createEntityManager();
     BaseDao<T> baseDao;
-    public void save(T entity){
+
+    public void save(T entity) {
+        entityManager.getTransaction().begin();
         baseDao.save(entity);
+        entityManager.getTransaction().commit();
     }
-    public void update(T entity){
+
+    public void update(T entity) {
+        entityManager.getTransaction().begin();
         baseDao.update(entity);
+        entityManager.getTransaction().commit();
     }
-    public void delete(T entity){
+
+    public void delete(T entity) {
+        entityManager.getTransaction().begin();
         baseDao.delete(entity);
+        entityManager.getTransaction().commit();
     }
-    public T loadById(Long id){
+
+    public T loadById(Long id) {
         return baseDao.loadById(id);
     }
+
     public List<T> loadAll() {
         return baseDao.loadAll();
     }
