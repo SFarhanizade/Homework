1c51b490f3582101f546874aa128358e675b8365
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-11-29 20:24:45 +0330

    builder pattern added.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/City.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/entity/City.java
@@ -26,4 +26,21 @@ public class City extends BaseThing{
     public void setStadiums(List<Stadium> stadiums) {
         this.stadiums = stadiums;
     }
+
+    public static CityBuilder builder(){
+        return new CityBuilder();
+    }
+
+    public static class CityBuilder{
+        private String name;
+
+        public CityBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public City build() {
+            return new City(null,name);
+        }
+    }
 }
