5630ff156bf6e37800261489b6fa7b4f76df5c5e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-01 13:56:48 +0330

    getAllSortedByPoints added.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/dao/TeamDao.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/dao/TeamDao.java
@@ -3,6 +3,8 @@ package dao;
 import entity.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class TeamDao extends BaseDao<Team>{
     public TeamDao(EntityManager entityManager) {
@@ -12,5 +14,11 @@ public class TeamDao extends BaseDao<Team>{
     @Override
     public Class<Team> getEntityClass() {
         return Team.class;
+    }
+
+    public List<Team> getAllSortedByPoints() {
+        TypedQuery<Team> query =
+                entityManager.createQuery("From Team t order by points", getEntityClass());
+        return query.getResultList();
     }
 }
--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/TeamManager.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/TeamManager.java
@@ -1,6 +1,12 @@
 package manager;
 
+import dao.TeamDao;
 import entity.Team;
 
+import java.util.List;
+
 public class TeamManager extends BaseManager<Team>{
+    public List<Team> getAllSortedByPoints() {
+        return ((TeamDao)baseDao).getAllSortedByPoints();
+    }
 }
