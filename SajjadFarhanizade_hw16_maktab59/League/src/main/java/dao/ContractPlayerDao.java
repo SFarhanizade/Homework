906b312237647bea5414457441dab67bdb475856
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-01 21:26:23 +0330

    getMostExpensive method and query added.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/dao/ContractPlayerDao.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/dao/ContractPlayerDao.java
@@ -1,8 +1,15 @@
 package dao;
 
+import entity.ContractCoach;
 import entity.ContractPlayer;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class ContractPlayerDao extends BaseDao<ContractPlayer> {
 
@@ -13,5 +20,20 @@ public class ContractPlayerDao extends BaseDao<ContractPlayer> {
     @Override
     public Class<ContractPlayer> getEntityClass() {
         return ContractPlayer.class;
+    }
+
+    public List<ContractPlayer> getMostExpensive() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<ContractPlayer> cq = cb.createQuery(ContractPlayer.class);
+        CriteriaQuery<Integer> cqP = cb.createQuery(Integer.class);
+        Root<ContractPlayer> from = cq.from(ContractPlayer.class);
+        Root<ContractPlayer> from1 = cqP.from(ContractPlayer.class);
+        cqP.select(cb.max(from1.get("price")));
+        TypedQuery<Integer> query1 = entityManager.createQuery(cqP);
+        int max = query1.getSingleResult();
+        cq.multiselect(from.get("player"),from.get("team"),from.get("price"), from.get("year"));
+        cq.where(cb.equal(from.get("price"),max));
+        TypedQuery<ContractPlayer> query = entityManager.createQuery(cq);
+        return query.getResultList();
     }
 }
--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/ContractPlayerManager.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/ContractPlayerManager.java
@@ -3,8 +3,14 @@ package manager;
 import dao.ContractPlayerDao;
 import entity.ContractPlayer;
 
+import java.util.List;
+
 public class ContractPlayerManager extends BaseManager<ContractPlayer> {
     public ContractPlayerManager() {
         baseDao = new ContractPlayerDao(entityManager);
+    }
+
+    public List<ContractPlayer> getMostExpensive() {
+        return ((ContractPlayerDao)baseDao).getMostExpensive();
     }
 }
