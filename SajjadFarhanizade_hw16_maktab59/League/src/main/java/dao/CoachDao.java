85e0be891a9a81f7640f2e2a7fddef8857ddca63
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-01 13:56:18 +0330

    loadFreeCoaches added.

--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/dao/CoachDao.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/dao/CoachDao.java
@@ -3,6 +3,8 @@ package dao;
 import entity.Coach;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class CoachDao extends BaseDao<Coach>{
     public CoachDao(EntityManager entityManager) {
@@ -12,5 +14,11 @@ public class CoachDao extends BaseDao<Coach>{
     @Override
     public Class<Coach> getEntityClass() {
         return Coach.class;
+    }
+
+    public List<Coach> loadFreeCoaches() {
+        TypedQuery<Coach> query =
+                entityManager.createQuery("From Coach c where c.team=null", getEntityClass());
+        return query.getResultList();
     }
 }
--- SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/CoachManager.java
+++ SajjadFarhanizade_hw16_maktab59/League/src/main/java/manager/CoachManager.java
@@ -1,6 +1,12 @@
 package manager;
 
+import dao.CoachDao;
 import entity.Coach;
 
+import java.util.List;
+
 public class CoachManager extends BaseManager<Coach> {
+    public List<Coach> loadFreeCoaches() {
+        return ((CoachDao) baseDao).loadFreeCoaches();
+    }
 }
