bca492a4965d80028786bb58f9058a4cd6fdadc8
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-12 17:09:33 +0330

    BookManager add and loadAll methods added.

--- SajjadFarhanizade_hw18_maktab59/BookStorage/src/main/java/ir/farhanizade/bookstorage/manager/BookManager.java
+++ SajjadFarhanizade_hw18_maktab59/BookStorage/src/main/java/ir/farhanizade/bookstorage/manager/BookManager.java
@@ -1,4 +1,19 @@
 package ir.farhanizade.bookstorage.manager;
 
+import ir.farhanizade.bookstorage.dao.BookDao;
+import ir.farhanizade.bookstorage.entity.Book;
+
+import java.io.IOException;
+import java.util.List;
+
 public class BookManager {
+    BookDao bookDao = new BookDao();
+
+    public void addBook(Book book) throws IOException {
+        bookDao.addBook(book);
+    }
+
+    public List<Book> loadAll() throws IOException {
+        return bookDao.loadAll();
+    }
 }
