042eb734bbc94a8b7ba964474319b1f3e6fa4451
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-12 17:06:45 +0330

    BookDao add and loadAll methods added.

--- SajjadFarhanizade_hw18_maktab59/BookStorage/src/main/java/ir/farhanizade/bookstorage/dao/BookDao.java
+++ SajjadFarhanizade_hw18_maktab59/BookStorage/src/main/java/ir/farhanizade/bookstorage/dao/BookDao.java
@@ -1,4 +1,32 @@
 package ir.farhanizade.bookstorage.dao;
 
+import ir.farhanizade.bookstorage.entity.Book;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookDao {
+    public void addBook(Book book) throws IOException {
+        FileOutputStream fos = new FileOutputStream("books.txt", true);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(book);
+        oos.close();
+    }
+
+    public List<Book> loadAll() throws IOException {
+        FileInputStream fis = new FileInputStream("books.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        ArrayList<Book> books = new ArrayList<>();
+        Book book;
+        try {
+            while (true) {
+                book = (Book) ois.readObject();
+                books.add(book);
+            }
+        } catch (ClassNotFoundException e) {
+            return books;
+        }
+
+    }
 }
