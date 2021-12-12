1fc1932304056caae46a9229b2d52a42f52325b3
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-12 16:56:18 +0330

    Properties added.

--- SajjadFarhanizade_hw18_maktab59/BookStorage/src/main/java/ir/farhanizade/bookstorage/entity/Book.java
+++ SajjadFarhanizade_hw18_maktab59/BookStorage/src/main/java/ir/farhanizade/bookstorage/entity/Book.java
@@ -1,4 +1,31 @@
 package ir.farhanizade.bookstorage.entity;
 
 public class Book {
+    private String title;
+    private String publisher;
+    private String author;
+    private Integer price;
+
+    public Book(String title, String publisher, String author, Integer price) {
+        this.title = title;
+        this.publisher = publisher;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
 }
