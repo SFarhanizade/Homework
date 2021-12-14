6624aebac0336db8d20c51e1c47edbdc8164c454
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-14 21:22:40 +0330

    file address changed

--- SajjadFarhanizade_hw18_maktab59/BookStorage/src/main/java/ir/farhanizade/bookstorage/dao/BookDao.java
+++ SajjadFarhanizade_hw18_maktab59/BookStorage/src/main/java/ir/farhanizade/bookstorage/dao/BookDao.java
@@ -1,34 +1,43 @@
 package ir.farhanizade.bookstorage.dao;
 
 import ir.farhanizade.bookstorage.entity.Book;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookDao {
-    String fileAddress = "//home//farhanizade//IdeaProjects//Homework//SajjadFarhanizade_hw18_maktab59//BookStorage//books.txt";
-    public void addBook(Book book) throws IOException {
+    String fileAddress = "/home/farhanizade/IdeaProjects/Homework/SajjadFarhanizade_hw18_maktab59/BookStorage/books.txt";
 
+    public void addBook(Book book) throws IOException {
         FileOutputStream fos = new FileOutputStream(fileAddress, true);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(book);
-        oos.close();
+        try {
+                oos.writeObject(book);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            oos.close();
+        }
     }
 
     public List<Book> loadAll() throws IOException {
         FileInputStream fis = new FileInputStream(fileAddress);
         ObjectInputStream ois = new ObjectInputStream(fis);
-        ArrayList<Book> books = new ArrayList<>();
-        Book book;
-        try {
-            while (true) {
-                book = (Book) ois.readObject();
+        List<Book> books = new ArrayList<>();
+        while (true) {
+            try {
+                Book book = (Book) ois.readObject();
                 books.add(book);
+            } catch (ClassNotFoundException e) {
+                break;
+            } catch (Exception e) {
+                break;
+            } finally {
+                return books;
+                //ois.close();
             }
-        } catch (ClassNotFoundException | EOFException e) {
-            return books;
         }
+
 
     }
 }
