cbd1306c0ecede57f5bb8af3fbdbcadf4ac3e1e9
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-18 11:50:13 +0330

    CityDao created.

---
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/dao/CityDao.java
@@ -0,0 +1,12 @@
+package ir.farhanizade.busticket.dao;
+
+import ir.farhanizade.busticket.core.BaseDao;
+import ir.farhanizade.busticket.entity.City;
+
+import javax.persistence.EntityManager;
+
+public class CityDao extends BaseDao<City> {
+    public CityDao(EntityManager entityManager) {
+        super(entityManager);
+    }
+}
