cecf4481f77c8057a6fac80cf7fe88d041843fe9
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-18 11:48:00 +0330

    Ticket properties added.

---
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/entity/Ticket.java
@@ -0,0 +1,64 @@
+package ir.farhanizade.busticket.entity;
+
+import ir.farhanizade.busticket.core.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Ticket extends BaseEntity {
+
+    @ManyToOne
+    private Travel travel;
+
+    @ManyToOne
+    private User owner;
+
+    public Ticket(Travel travel, User owner) {
+        this.travel = travel;
+        this.owner = owner;
+    }
+
+    public Ticket() {
+    }
+
+    public Travel getTravel() {
+        return travel;
+    }
+
+    public void setTravel(Travel travel) {
+        this.travel = travel;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public static TicketBuilder builder(){
+        return new TicketBuilder();
+    }
+
+    static class TicketBuilder{
+        private Travel travel;
+        private User owner;
+
+        public TicketBuilder travel(Travel travel){
+            this.travel = travel;
+            return this;
+        }
+
+        public TicketBuilder owner(User owner){
+            this.owner = owner;
+            return this;
+        }
+
+        public Ticket build(){
+            return new Ticket(travel,owner);
+        }
+    }
+}
