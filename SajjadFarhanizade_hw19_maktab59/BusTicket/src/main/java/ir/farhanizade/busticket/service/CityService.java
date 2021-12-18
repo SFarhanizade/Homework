8ff71f447cded3261fa06a58affa425fcf7c0e21
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-18 11:58:12 +0330

    CityService created.

---
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/service/CityService.java
@@ -0,0 +1,7 @@
+package ir.farhanizade.busticket.service;
+
+import ir.farhanizade.busticket.core.BaseService;
+import ir.farhanizade.busticket.entity.City;
+
+public class CityService extends BaseService<City> {
+}
