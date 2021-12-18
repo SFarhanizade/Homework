87036da7813295469412aaa2b499ab5ad94cd316
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-18 10:58:50 +0330

    BaseDao CRUD operation added.

---
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/core/BaseDao.java
@@ -0,0 +1,32 @@
+package ir.farhanizade.busticket.core;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public abstract class BaseDao<T extends BaseEntity> {
+    private EntityManager entityManager;
+
+    public void saveOrUpdate(T entity) {
+        entityManager.getTransaction().begin();
+        if (entity.getId() == null)
+            entityManager.persist(entity);
+        else
+            entityManager.merge(entity);
+        entityManager.getTransaction().commit();
+    }
+
+    public T loadById(Integer id) {
+        T result = entityManager.find(getEntityClass(), id);
+        return result;
+    }
+
+    public List<T> loadAll() {
+        TypedQuery<T> query = entityManager.createQuery("From " + getEntityClass().getSimpleName(),
+                getEntityClass());
+        List<T> resultList = query.getResultList();
+        return resultList;
+    }
+
+    public abstract Class<T> getEntityClass();
+}
