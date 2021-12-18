da0bf36bccb00ad3e0a53c3b3c786a684c7cda02
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-18 13:39:43 +0330

    saveOrUpdate method overridden.

--- SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/dao/TicketDao.java
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/dao/TicketDao.java
@@ -2,12 +2,27 @@ package ir.farhanizade.busticket.dao;
 
 import ir.farhanizade.busticket.core.BaseDao;
 import ir.farhanizade.busticket.entity.Ticket;
+import ir.farhanizade.busticket.entity.User;
 
 import javax.persistence.EntityManager;
 
 public class TicketDao extends BaseDao<Ticket> {
     public TicketDao(EntityManager entityManager) {
         super(entityManager);
+    }
+
+    @Override
+    public void saveOrUpdate(Ticket entity) {
+        entityManager.getTransaction().begin();
+        if (entity.getId() == null)
+            entityManager.persist(entity);
+        else
+            entityManager.merge(entity);
+        User owner = entity.getOwner();
+        owner.addTicket(entity);
+        entityManager.persist(owner);
+        entityManager.getTransaction().commit();
+
     }
 
     @Override
