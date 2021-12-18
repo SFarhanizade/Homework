bd73cc452cc0ec0fef136d76354b1b4d062b3350
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-18 11:52:09 +0330

    TicketDao created.

---
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/dao/TicketDao.java
@@ -0,0 +1,12 @@
+package ir.farhanizade.busticket.dao;
+
+import ir.farhanizade.busticket.core.BaseDao;
+import ir.farhanizade.busticket.entity.Ticket;
+
+import javax.persistence.EntityManager;
+
+public class TicketDao extends BaseDao<Ticket> {
+    public TicketDao(EntityManager entityManager) {
+        super(entityManager);
+    }
+}
