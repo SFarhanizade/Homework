cd4bd51e6c51ec25ef129324f9e15f81e6468e91
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-18 11:59:10 +0330

    TravelService added.

---
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/service/TravelService.java
@@ -0,0 +1,7 @@
+package ir.farhanizade.busticket.service;
+
+import ir.farhanizade.busticket.core.BaseService;
+import ir.farhanizade.busticket.entity.Travel;
+
+public class TravelService extends BaseService<Travel> {
+}
