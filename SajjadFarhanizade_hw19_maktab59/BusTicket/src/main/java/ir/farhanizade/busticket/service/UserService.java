7db5b62c0f6e22ec8f2420d735f791b2cb146cab
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-18 11:59:45 +0330

    UserService added.

---
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/service/UserService.java
@@ -0,0 +1,7 @@
+package ir.farhanizade.busticket.service;
+
+import ir.farhanizade.busticket.core.BaseService;
+import ir.farhanizade.busticket.entity.User;
+
+public class UserService extends BaseService<User> {
+}
