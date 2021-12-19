a5f6e8c0bbf9c5174d0df2cd40289df10999e7b0
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-19 22:42:43 +0330

    Login method added.

--- SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/dao/AdminDao.java
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/dao/AdminDao.java
@@ -2,8 +2,11 @@ package ir.farhanizade.busticket.dao;
 
 import ir.farhanizade.busticket.core.BaseDao;
 import ir.farhanizade.busticket.entity.Admin;
+import ir.farhanizade.busticket.entity.User;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class AdminDao extends BaseDao<Admin> {
     public AdminDao(EntityManager entityManager) {
@@ -13,5 +16,17 @@ public class AdminDao extends BaseDao<Admin> {
     @Override
     public Class<Admin> getEntityClass() {
         return Admin.class;
+    }
+
+    public Admin login(String username, String password) {
+        TypedQuery<Admin> query = entityManager.createQuery(
+                "From Admin a Where a.username=:username AND a.password=:password",
+                getEntityClass());
+        query.setParameter("username", username);
+        query.setParameter("password", password);
+        List<Admin> resultList = query.getResultList();
+        if (resultList.size() == 1)
+            return resultList.get(0);
+        return null;
     }
 }
--- SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/service/AdminService.java
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/service/AdminService.java
@@ -2,10 +2,15 @@ package ir.farhanizade.busticket.service;
 
 import ir.farhanizade.busticket.core.BaseService;
 import ir.farhanizade.busticket.dao.AdminDao;
+import ir.farhanizade.busticket.dao.UserDao;
 import ir.farhanizade.busticket.entity.Admin;
 
 public class AdminService extends BaseService<Admin> {
     public AdminService() {
         baseDao = new AdminDao(entityManager);
+    }
+
+    public Admin login(String username, String password) {
+        return ((AdminDao)baseDao).login(username, password);
     }
 }
