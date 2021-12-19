b678a50ea9267d7c1ee083357e7a76dff0bade6e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-19 16:11:29 +0330

    getTravels method added.

--- SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/dao/TravelDao.java
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/dao/TravelDao.java
@@ -4,10 +4,25 @@ import ir.farhanizade.busticket.core.BaseDao;
 import ir.farhanizade.busticket.entity.Travel;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.time.LocalDate;
+import java.util.List;
 
 public class TravelDao extends BaseDao<Travel> {
     public TravelDao(EntityManager entityManager) {
         super(entityManager);
+    }
+
+    public List<Travel> getTravels(int origin, int destination, LocalDate localDate) {
+        TypedQuery<Travel> query = entityManager.createQuery(
+                "From Travel t " +
+                        "where t.origin.id=:origin and t.destination.id=:destination and t.date=:localDate"
+                , getEntityClass());
+        query.setParameter("origin", origin);
+        query.setParameter("destination", destination);
+        query.setParameter("localDate", localDate);
+        List<Travel> resultList = query.getResultList();
+        return resultList;
     }
 
     @Override
--- SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/service/TravelService.java
+++ SajjadFarhanizade_hw19_maktab59/BusTicket/src/main/java/ir/farhanizade/busticket/service/TravelService.java
@@ -4,8 +4,15 @@ import ir.farhanizade.busticket.core.BaseService;
 import ir.farhanizade.busticket.dao.TravelDao;
 import ir.farhanizade.busticket.entity.Travel;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class TravelService extends BaseService<Travel> {
     public TravelService() {
-        baseDao= new TravelDao(entityManager);
+        baseDao = new TravelDao(entityManager);
+    }
+
+    public List<Travel> getTravels(int origin, int destination, LocalDate localDate) {
+        return ((TravelDao) baseDao).getTravels(origin, destination, localDate);
     }
 }
